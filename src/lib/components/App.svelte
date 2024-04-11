8bd3d0f00726932a57a3759709efb750232982c8
ev <ev@1nf.me>
2024-04-11 01:32:17 +0100

    feat: buttons

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -4,6 +4,10 @@
 	import { tweened } from 'svelte/motion'
 	import { fade } from 'svelte/transition'
 	import Scene from './Scene.svelte'
+
+	import { Github, Globe } from 'lucide-svelte'
+	import Button from './Button.svelte'
+
 	const { progress } = useProgress()
 	const tweenedProgress = tweened($progress, {
 		duration: 800
@@ -16,22 +20,26 @@
 		transition:fade|local={{
 			duration: 200
 		}}
-		class="absolute w-full h-full bg-black flex flex-col gap-1.5 items-center justify-center text-white"
+		class="absolute flex h-full w-full flex-col items-center justify-center gap-1.5 bg-black text-white"
 	>
 		<p class="text-xl">Loading</p>
-		<div class="w-1/3 h-3 border border-white relative">
+		<div class="relative h-3 w-1/3 border border-white">
 			<div class="h-full bg-white" style="width: {$tweenedProgress * 100}%" />
 		</div>
 	</div>
 {/if}
-<div class="h-full relative antialiased">
-	<div class="absolute left-10 top-10 text-white z-0">
-		<div class="text-2xl font-bold pl-2 pb-4">Lockheed Martin Corporation</div>
+<div class="relative h-full antialiased">
+	<div class="absolute left-10 top-10 text-white">
+		<div class="pb-4 pl-2 text-2xl font-bold">Lockheed Martin Corporation</div>
 		<div class="text-9xl font-black">F-22A</div>
 		<div class="text-8xl font-semibold">Raptor</div>
-		<div class="text-xl font-semibold pl-2 pt-4">
+		<div class="pl-2 pt-4 text-xl font-semibold">
 			A twin engine, all-weather, stealth air-superiority fighter aircraft.
 		</div>
+	</div>
+	<div class="absolute bottom-5 right-5 space-x-2">
+		<Button href="https://github.com/evwltrs/jet"><Github /></Button>
+		<Button href="https://1nf.me"><Globe /></Button>
 	</div>
 	<Canvas>
 		<Scene />
---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let href
+</script>
+
+<a
+	{href}
+	class="group relative inline-flex items-center justify-center overflow-hidden rounded-md px-6 py-3 font-bold text-white shadow-2xl"
+>
+	<span
+		class="absolute inset-0 h-full w-full bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 opacity-0 transition duration-300 ease-out group-hover:opacity-100"
+	></span>
+	<!-- Top glass gradient -->
+	<span
+		class="absolute left-0 top-0 h-1/3 w-full bg-gradient-to-b from-white to-transparent opacity-5"
+	></span>
+	<!-- Bottom gradient -->
+	<span
+		class="absolute bottom-0 left-0 h-1/3 w-full bg-gradient-to-t from-white to-transparent opacity-5"
+	></span>
+	<!-- Left gradient -->
+	<span
+		class="absolute bottom-0 left-0 h-full w-4 bg-gradient-to-r from-white to-transparent opacity-5"
+	></span>
+	<!-- Right gradient -->
+	<span
+		class="absolute bottom-0 right-0 h-full w-4 bg-gradient-to-l from-white to-transparent opacity-5"
+	></span>
+	<span class="absolute inset-0 h-full w-full rounded-md border border-white opacity-10"></span>
+	<span
+		class="absolute h-0 w-0 rounded-full bg-white opacity-5 transition-all duration-300 ease-out group-hover:h-56 group-hover:w-56"
+	></span>
+	<span class="relative"><slot /></span>
+</a>
